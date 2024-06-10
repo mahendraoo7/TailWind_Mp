d385f32782d5faa56758f4e0c02e4cbefcc82ee5
Mahendra <mahendrapurohit9889@gmail.com>
2024-06-10 08:57:26 +0530

    10-06-2024

---
+++ Lecture1/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: { 
+        'hero-pattern': "url('/img/hero-pattern.svg')",
+        'footer-texture': "url('/img/footer-texture.png')",
+      }
+    },
+  },
+  plugins: [],
+}
